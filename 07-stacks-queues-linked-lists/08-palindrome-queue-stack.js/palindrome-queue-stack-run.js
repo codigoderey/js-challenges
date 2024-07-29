f2c7054899db12dc429ba3ddc93c09def947b79b
Reynaldo Navedo <codigoderey@outlook.com>
2024-07-29 19:23:13 -0400

    minor update

--- 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack-run.js
+++ 07-stacks-queues-linked-lists/08-palindrome-queue-stack.js/palindrome-queue-stack-run.js
@@ -3,4 +3,4 @@ const isPalindromeQueueStack = require("./palindrome-queue-stack");
 const result1 = isPalindromeQueueStack("A man, a plan, a canal: Panama");
 const result2 = isPalindromeQueueStack("Hello");
 
-result1, result2;
+console.log(result1, result2);
