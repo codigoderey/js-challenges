c7c7c24fcfd9b8ec7d3bab54bee0bee73d386d47
Reynaldo Navedo <codigoderey@outlook.com>
2024-08-21 21:09:49 -0400

    progress made

--- 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
+++ 02-basic-challenges-2/06-are-all-chars-unique/are-all-chars-unique.js
@@ -1,3 +1,37 @@
-function areAllCharactersUnique() {}
+// function areAllCharactersUnique(str) {
+//     // convert the string into an interable array
+//     const charSet = new Set()
+
+//     // loop through the string
+//     for(let i = 0; i < str.length; i++){
+//         // current character
+//         const char = str[i]
+//         if(charSet.has(char)){
+//             return false
+//         }
+//         charSet.add(char)
+//     }
+
+//     return true
+
+// }
+
+function areAllCharactersUnique(str) {
+    // convert the string into an interable array
+    const charSet = []
+
+    // loop through the string
+    for(let i = 0; i < str.length; i++){
+        // current character
+        const char = str[i]
+        if(charSet.includes(char)){
+            return false
+        }
+        charSet.push(char)
+    }
+
+    return true
+
+}
 
 module.exports = areAllCharactersUnique;
