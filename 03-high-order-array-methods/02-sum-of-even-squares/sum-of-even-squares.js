d03906e1ea53467351b7bae2ed9597363911bf83
Reynaldo Navedo <codigoderey@outlook.com>
2024-08-27 18:34:50 -0400

    progress made with high order functions

--- 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
+++ 03-high-order-array-methods/02-sum-of-even-squares/sum-of-even-squares.js
@@ -1,3 +1,29 @@
-function sumOfEvenSquares() {}
+// Solution #1
+// function sumOfEvenSquares(arr) {
+
+//     // initialize sum
+//     let total = 0;
+
+//     // filter the array to get even numbers
+//     let squareNumbers = arr.filter(n => n % 2 === 0)
+
+//     // numbers
+//     for(var i = 0; i < squareNumbers.length; i++){
+//         total = total + squareNumbers[i] ** 2
+//     }
+
+//     return total
+
+// }
+
+// Solution #2
+function sumOfEvenSquares(arr){
+
+    let squareNumbers = arr.filter(n => n % 2 == 0 )
+
+    let total = squareNumbers.reduce((acc, curr) => acc + curr ** 2 , 0)
+
+    return total
+}
 
 module.exports = sumOfEvenSquares;
--- 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
+++ 03-high-order-array-methods/03-calculate-total-sales/calculate-total-sales.js
@@ -1,3 +1,16 @@
-function calculateTotalSalesWithTax() {}
+function calculateTotalSalesWithTax(arr, tax = 8) {
+    let totalPrice = 0;
+
+    tax = 1 + tax / 100
+    
+    for(i = 0; i < arr.length; i++){ 
+        totalPrice = totalPrice + arr[i].price * arr[i].quantity
+    }
+
+    let priceWithTax = totalPrice * tax
+
+    return priceWithTax
+
+}
 
 module.exports = calculateTotalSalesWithTax;
